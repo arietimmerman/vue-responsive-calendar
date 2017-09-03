fdbc9bf686e4036c792e7deb2fb1173e29430867
Arie Timmerman <arietimmerman@Aries-MacBook-Pro.local>
2017-09-03 21:03:11 +0200

    fixed paths

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   entry: ['./src/index.js'],
   output: {
     path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist/',
+    publicPath: '../dist/',
     filename: 'calendar.min.js',
     library: 'ResponsiveCalendar',
     libraryTarget: 'umd'
