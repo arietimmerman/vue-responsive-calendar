86e24603854b3d17e351f3c53d730cd8ea12ebe9
Arie Timmerman <arietimmerman@Aries-MacBook-Pro.local>
2017-10-08 15:30:34 +0200

    fixed module exporting

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-export { default } from './dist/vue-ical-calendar.common'
-export * from './dist/vue-ical-calendar.common'
-import './dist/vue-ical-calendar.css'
+export { default } from './dist/vue-responsive-calendar.common'
+export * from './dist/vue-responsive-calendar.common'
+import './dist/vue-responsive-calendar.css'
